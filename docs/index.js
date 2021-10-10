e1b73bc27c1c19ce47d0115f763699f870b59683
Bartosz Jażdżyk <bartek.jazdzyk.007@gmail.com>
2021-10-10 17:56:40 +0200

    terrain render

--- docs/index.js
+++ docs/index.js
@@ -7,6 +7,32 @@ let _W = window.innerWidth
 let _H = window.innerHeight
 c.width = _W
 c.height = _H
-ctx.moveTo(0, 0);
-ctx.lineTo(_W, _H);
-ctx.stroke();
+let Terrain = [[1, 1, 1, 1, 1],
+	[1, 1, 1, 1],
+	[1, 1, 1, 1, 1],
+	[1, 1, 1],
+	[1],
+	[1]]
+
+let tick = 0
+const loop=()=>{
+	tick += 1
+	requestAnimationFrame(loop)
+	//clear screen
+	ctx.fillStyle = "#0000FF"
+	ctx.fillRect(0, 0, _W, _H)
+
+	//render Tarrain
+	for(let i=0; i<Terrain.length; i++){
+		for(let j=0; j<Terrain[i].length; j++){
+			if(Terrain[i][j] == 1){
+				ctx.fillStyle = "#19bf1e"
+				ctx.strokeStyle = "#000000"
+				ctx.fillRect(j*50, i*50, 50, 50)
+				ctx.strokeRect(j*50, i*50, 50, 50)
+			}
+		}
+	}
+
+	}
+loop()
