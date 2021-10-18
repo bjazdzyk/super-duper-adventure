a6e8b104b912ba2395a1d8db4ef01bdc89772169
Bartosz Jażdżyk <bartek.jazdzyk.007@gmail.com>
2021-10-18 19:41:14 +0200

    added cursor

--- docs/index.js
+++ docs/index.js
@@ -1,9 +1,15 @@
 var c = document.getElementById("myCanvas");
 var ctx = c.getContext("2d");
 
-const drawHexagon =(x, y, s, color)=>{
-	ctx.fillStyle = color
-	ctx.strokeStyle = "#000000"
+const drawHexagon =(x, y, s, color, type = "normal")=>{
+	if(type == "normal"){
+		ctx.fillStyle = color
+		ctx.strokeStyle = "#000000"
+		ctx.lineWidth = 1
+	}else if(type == "cursor"){
+		ctx.lineWidth = 5
+		ctx.strokestyle = "#000000"
+	}
 	ctx.beginPath()
 	ctx.moveTo(x+s/2, y)
 	ctx.lineTo(x+s, y+s/4)
@@ -13,13 +19,23 @@ const drawHexagon =(x, y, s, color)=>{
 	ctx.lineTo(x, y+s/4)
 	ctx.lineTo(x+s/2, y)
 	ctx.closePath()
-	ctx.fill()
-	ctx.stroke()
+	if(type == "normal"){
+		ctx.stroke()
+		ctx.fill()
+	}else if(type == "cursor"){
+		ctx.stroke()
+	}
+}
+
+const coords =(x, y)=>{
+	return {x: (y-x), y:(y+x)}
 }
 
 //terrain generation
+
 var simplex = new SimplexNoise()
-let krat = 70
+let krat = 50
+
 const generate =()=>{
 	let T = []
 	for(let i=0; i<krat; i++){
@@ -37,36 +53,75 @@ const generate =()=>{
 
 
 let T = generate()
-console.log(T)
-
 let tick = 0
+let lastTick = 0
+let focus = {x:Math.floor(krat/2), y:Math.floor(krat/2)}
+let cursor = {x:0, y:0}
+let d = 1
 const loop=()=>{
+	tick += 1
+	lastTick
+	requestAnimationFrame(loop)
+	if(tick-lastTick>3){
+		if(keys["ArrowLeft"]){
+			lastTick = tick
+			cursor.y -=1
+			cursor.x +=1
+		}if(keys["ArrowRight"]){
+			lastTick = tick
+			cursor.y +=1
+			cursor.x -=1
+		}if(keys["ArrowUp"]){
+			lastTick = tick
+			cursor.x -= (d+1)%2
+			cursor.y -= d%2
+			d+=1
+			console.log(cursor.x,cursor.y)
+		}if(keys["ArrowDown"]){
+			lastTick = tick
+			cursor.x += (d+1)%2
+			cursor.y += d%2
+			d+=1
+		}
+	}
+
 	let _W = window.innerWidth
 	let _H = window.innerHeight
 	c.width = _W
 	c.height = _H
-	tick += 1
-	requestAnimationFrame(loop)
+	let a = _H/krat*2
+
+	let offsetH = _H/2-a*coords(focus.x, focus.y).y-a*3/4
+	let offsetW = _W/2-a*coords(focus.x, focus.y).x-a
+
 	//clear screen
 	ctx.fillStyle = "#0000FF"
 	ctx.fillRect(0, 0, _W, _H)
 
-	let offsetH = -_H
-	let offsetW = _W/2
-
 	//render Tarrain
 	for(let i=0; i<T.length; i++){
 		for(let j=0; j<T[i].length; j++){
-			let a = _W/krat
-			
 			if(T[i][j] == 1){
-				drawHexagon(offsetW+a*(j-i), offsetH+a*(j+i), a*2, "#19bf1e")
+				drawHexagon(offsetW+a*coords(i, j).x, offsetH+a*coords(i, j).y, a*2, "#19bf1e")
 			}
 			if(T[i][j] >= 2){
-				drawHexagon(offsetW+a*(j-i), offsetH+a*(j+i), a*2, "darkgreen")
+				drawHexagon(offsetW+a*coords(i, j).x, offsetH+a*coords(i, j).y, a*2, "darkgreen")
 			}
 		}
 	}
-
-	}
+	drawHexagon(_W/2+a*coords(cursor.x, cursor.y).x-a, _H/2+a*coords(cursor.x, cursor.y).y-a*3/4, a*2, "black", "cursor")
+	
+	
+	
+}
+//events
+let keys = {}
+document.addEventListener('keydown', keyDown);
+document.addEventListener('keyup', keyUp);
+function keyDown(e) {
+	keys[e.code] = true
+}
+function keyUp(e) {
+	keys[e.code] = null
+}
 loop()
